32a186030bcc2fb7993db486536b6d3d3f7decaa
Michael Termotto <mjtermotto11@gmail.com>
2023-10-18 00:08:45 +0200

    Added recursive function

--- romanNumerals.js
+++ romanNumerals.js
@@ -14,23 +14,23 @@ const roman_numerals=[
   {name:"M",value:1000}
 ];
 
-// function toRomanLazy(num) {
-//   let roman = ""
-//   for(let x = roman_numerals.length-1; x >= 0; x--){
-//     if(roman_numerals[x].isSpecial)
-//       continue;
+function toRomanLazy(num) {
+  let roman = ""
+  for(let x = roman_numerals.length-1; x >= 0; x--){
+    if(roman_numerals[x].isSpecial)
+      continue;
 
-//     amount_divides = Math.floor(num / roman_numerals[x].value);
+    amount_divides = Math.floor(num / roman_numerals[x].value);
 
 
-//     if(amount_divides > 0){
-//       roman = roman + roman_numerals[x].name.repeat(amount_divides)
-//       num = num - (amount_divides * roman_numerals[x].value)
-//       if(num == 0) return roman;
-//     }
-//   }
-//   return "Error";
-// }
+    if(amount_divides > 0){
+      roman = roman + roman_numerals[x].name.repeat(amount_divides)
+      num = num - (amount_divides * roman_numerals[x].value)
+      if(num == 0) return roman;
+    }
+  }
+  return "Error";
+}
 
 function toRoman(num, isLazy = true) {
   let roman = ""
@@ -52,6 +52,37 @@ function toRoman(num, isLazy = true) {
   return "Error";
 }
 
-console.log(toRoman(154,false));
+
+
+function toRomanRecursive(roman_string, input_num, current_roman, isLazy = true) {
+
+    if(roman_numerals[current_roman].isSpecial && isLazy)
+      return toRomanRecursive(roman_string, input_num, current_roman - 1, isLazy)
+
+
+    amount_divides = Math.floor(input_num / roman_numerals[current_roman].value);
+
+    if(amount_divides > 0){
+      roman_string = roman_string + roman_numerals[current_roman].name.repeat(amount_divides)
+      input_num = input_num - (amount_divides * roman_numerals[current_roman].value)
+
+      if(input_num == 0) return roman_string;
+      else return toRomanRecursive(roman_string, input_num, current_roman - 1, isLazy)
+    }
+    else{
+      console.log(roman_string)
+      return toRomanRecursive(roman_string, input_num, current_roman - 1, isLazy)
+    }
+  
+}
+
+
+
+
+console.log(toRoman(1549, true));
+
+
+console.log(toRomanRecursive("", 1549,roman_numerals.length-1, true));
 
 module.exports = { toRoman, toRomanLazy };
+
